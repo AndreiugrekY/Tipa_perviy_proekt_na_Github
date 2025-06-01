439edaf0e0e389d766b499b92848cd9a1c10f8c7
AndreiugrekY <andreiugreky@gmail.com>
2025-06-01 20:03:06 +0800

    обновил кнопки

--- botkino.py
+++ botkino.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+import random
 
 TOKEN = "7540140019:AAG7heZuusDAw-P9NQoukiRvQjKkIzbpbWw"
 bot = telebot.TeleBot(TOKEN)
@@ -18,3 +19,38 @@ def start(msg):
         "Привет! Я бот, в котором ты можешь выбрать фильм на любой вкус! /n /n Выбирай жанр ниже 👇",
         reply_markup=mrk
     )
+
+@bot.message_handler(content_types = ["text"])
+def buttons(msg):
+    film_b = random.choice()
+    if msg.text == "🔫 Боевик":
+        bot.send_message(
+            msg.chat.id,
+            f"Предлагаю посмотреть фильм", film_b, "жанра Боевик"
+        )
+
+    film_k = random.choice()
+    if msg.text == "🤣 Комедия":
+        bot.send_message(
+            msg.chat.id,
+            f"Предлагаю посмотреть фильм", film_k, "жанра Комедия"
+        )
+
+    film_f = random.choice()
+    if msg.text == "👽 Фантастика":
+        bot.send_message(
+            msg.chat.id,
+            f"Предлагаю посмотреть фильм", film_f, "жанра Фантастика"
+        )
+
+    film_y = random.choice()
+    if msg.text == "👻 Ужасы":
+        bot.send_message(
+            msg.chat.id,
+            f"Предлагаю посмотреть фильм", film_y, "жанра Ужасы"
+        )
+
+    if msg.text == "🤖 Перезапустить бота":
+        start(msg)
+
+bot.polling()
